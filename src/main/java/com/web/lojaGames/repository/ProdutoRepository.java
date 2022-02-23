36fb94b5b8e4f8031405868e7fb04fb0075550aa
unknown <marialuiza.filinto@gmail.com>
2022-02-23 11:53:28 -0300

    @malufilinto add produto repository

---
+++ src/main/java/com/web/lojaGames/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.web.lojaGames.repository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.web.lojaGames.Model.Categoria;
+import com.web.lojaGames.Model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	public List<Produto> findAllByDescricaoContainingIgnoreCase(String descricao);
+	
+	public List<Produto> findAllByValor(Double valor);
+}

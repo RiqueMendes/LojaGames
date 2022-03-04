faf177877e940c03d3c95a215ce291b29504b201
Paulo <analu.lamas@yahoo.com.br>
2022-03-04 14:00:04 -0300

    @nalulamas

---
+++ src/main/java/com/web/lojaGames/Model/UserLogin.java
@@ -0,0 +1,47 @@
+package com.web.lojaGames.Model;
+
+public class UserLogin {
+
+	private String name;	
+
+	private String password;	
+	
+	private String token;
+	
+	private String user;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	
+
+}
